29d4316d6b663761a6d3a719aa39212eaac328b8
Yusup <hunzah9@mail.ru>
2023-10-10 11:41:22 +0300

    add choose number logic

--- script.js
+++ script.js
@@ -1,101 +1,140 @@
-let name = '';
-let number = Math.floor(Math.random() * 100);
-let guesses = 0;
-let wrongGuesses = 0;
-let isHint = false;
+document.addEventListener('DOMContentLoaded', function() {
+    let name = '';
+    let number = 0;
+    let guesses = 0;
+    let wrongGuesses = 0;
+    let isHint = false;
 
-const output = document.querySelector('#output');
-const prompt = document.querySelector('#prompt');
-const input = document.querySelector('#prompt input');
+    const output = document.querySelector('#output');
+    const prompt = document.querySelector('#prompt');
+    const input = document.querySelector('#prompt input');
 
-prompt.addEventListener('submit', handleSubmit);
-
-
-
-
-const customNumberCheckbox = document.querySelector('#customNumberCheckbox');
-const customNumberInputContainer = document.querySelector('#customNumberInputContainer');
-const customNumberInput = document.querySelector('#customNumber');
-const guessNumberText = document.querySelector('#guessNumberText')
-
-customNumberCheckbox.addEventListener('change', (event) => {
-    if (customNumberCheckbox.checked) {
-        customNumberInputContainer.style.display = 'block'; // Показать инпут, если чекбокс отмечен
-        guessNumberText.style.display = 'block';
-    } else {
-        customNumberInputContainer.style.display = 'none'; // Скрыть инпут, если чекбокс не отмечен
-        guessNumberText.style.display = 'none';
-    }
-});
-
-printMessage('Введите имя игрока:');
-
-
-input.focus();
-
-function handleSubmit (event) {
-    event.preventDefault()
-    processInput(input.value);
-    input.value = ''
-}
-
-
-function printMessage(message, isHint = false) {
-    let li = document.createElement('li');
-    li.textContent = message;
-    
-    if (isHint) {
-        li.classList.add('hint'); 
-    }
-    
-    output.appendChild(li);
-}
+    prompt.addEventListener('submit', handleSubmit);
 
 
 
-function clearOutput() {
-    for (let i = 0; i < output.children.length; i++) {
-        output.removeChild(output.children[i]);
-    }
-}
+    const customNumberCheckbox = document.querySelector('#customNumberCheckbox');
+    const customNumberInputContainer = document.querySelector('#customNumberInputContainer');
+    const checkboxContainer = document.querySelector('.guess-checkbox-container')
+    const customNumberInput = document.querySelector('#customNumber');
+    const numberErrorMessage = document.querySelector('#numberErrorMessage');
+    const guessNumberText = document.querySelector('.guess-checkbox-container #guessNumberText');
+    const guessNumberTextChecked = document.querySelector('.guess-checkbox-container #guessNumberTextChecked');
 
-function processInput(inputText) {
-    if (!inputText) return;
+    customNumberCheckbox.addEventListener('change', (event) => {
+        if (customNumberCheckbox.checked) {
+            customNumberInputContainer.style.display = 'block'; 
+            guessNumberText.style.display = 'none';
+            guessNumberTextChecked.style.display = 'block'; 
+            
+        } else {
+            customNumberInputContainer.style.display = 'none'; 
+            guessNumberText.style.display = 'block';
+            guessNumberTextChecked.style.display = 'none'; 
+        }
+    });
 
-    if (!name) {
-        name = inputText;
-        clearOutput();
-        printMessage(`${name}, загадано число от 0 до 100. Попробуй отгадать его за наименьшее количество попыток. После каждой попытки я скажу "мало", "много" или "верно".`);
-        return;
-    }
 
-    printMessage(inputText);
+    printMessage('Введите имя игрока:');
 
-    let guess = Number.parseInt(inputText);
 
-    if (Number.isNaN(guess)) return;
-
-    guesses += 1;
-
-    if (guess > number) {
-        printMessage('Много. Попробуй еще раз.');
-    } else if (guess < number) {
-        printMessage('Мало. Попробуй еще раз.');
-    } else {
-        printMessage(`Верно, это число ${guess}.`);
-        printMessage(`Количество попыток: ${guesses}.`);
-        printMessage('GAME OVER');
-        prompt.remove();
-    }
-
-    if (guess !== number) {
-        wrongGuesses += 1;
-        if (wrongGuesses === 3) {
-            if (number % 2 === 0) {
-                printMessage('Подсказка: Загаданное число является четным.', true);
+    customNumberInput.addEventListener('keydown', (event) => {
+        if (event.key === "Enter") { 
+            event.preventDefault(); 
+            
+            const inputValue = +event.target.value;
+            if (inputValue === "") {
+                number = Math.floor(Math.random() * 1000);
+                customNumberInput.disabled = false; 
+                numberErrorMessage.textContent = ""; 
+                checkboxContainer.style.display = 'none'
+                customNumberInputContainer.style.display = 'none'
             } else {
-                printMessage('Подсказка: Загаданное число является нечетным.', true);
+                const parsedValue = parseInt(inputValue);
+                if (parsedValue < 0) {
+                    numberErrorMessage.textContent = "Число должно быть больше 0";
+                    event.target.value = "";
+                } else if (parsedValue > 1000) {
+                    numberErrorMessage.textContent = "Число должно быть меньше 1000";
+                    event.target.value = ""; 
+                } else {
+                    number = parsedValue;
+                    customNumberInput.disabled = false; 
+                    numberErrorMessage.textContent = ""; 
+                    checkboxContainer.style.display = 'none'
+                    customNumberInputContainer.style.display = 'none'
+                }
+            }
+        }
+    });
+    
+
+
+    input.focus();
+
+    function handleSubmit (event) {
+        event.preventDefault()
+        processInput(input.value);
+        input.value = ''
+    }
+
+
+    function printMessage(message, isHint = false) {
+        let li = document.createElement('li');
+        li.textContent = message;
+        
+        if (isHint) {
+            li.classList.add('hint'); 
+        }
+        
+        output.appendChild(li);
+    }
+
+
+
+    function clearOutput() {
+        for (let i = 0; i < output.children.length; i++) {
+            output.removeChild(output.children[i]);
+        }
+    }
+
+    function processInput(inputText) {
+        if (!inputText) return;
+
+        if (!name) {
+            name = inputText;
+            clearOutput();
+            printMessage(`${name}, загадано число от 0 до 1000. Попробуй отгадать его за наименьшее количество попыток. После каждой попытки я скажу "мало", "много" или "верно".`);
+            return;
+        }
+
+        printMessage(inputText);
+
+        let guess = Number.parseInt(inputText);
+
+        if (Number.isNaN(guess)) return;
+
+        guesses += 1;
+
+        if (guess > number) {
+            printMessage('Много. Попробуй еще раз.');
+        } else if (guess < number) {
+            printMessage('Мало. Попробуй еще раз.');
+        } else {
+            printMessage(`Верно, это число ${guess}.`);
+            printMessage(`Количество попыток: ${guesses}.`);
+            prompt.remove();
+        }
+
+        if (guess !== number) {
+            wrongGuesses += 1;
+            if (wrongGuesses === 3) {
+                if (number % 2 === 0) {
+                    printMessage('Подсказка: Загаданное число является четным.', true);
+                } else {
+                    printMessage('Подсказка: Загаданное число является нечетным.', true);
+                }
             }
         }
     }
-}
+})
